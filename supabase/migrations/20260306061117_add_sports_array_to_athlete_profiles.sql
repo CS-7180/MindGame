5cc5dba399222f6b9422cc3c86f747669b9783a8
RajLaskar10 <raj.laskar2016@gmail.com>
2026-03-07 11:46:42 -0500

    chore(supabase): squash structural migrations into a single remote schema dump to prevent conflicts

--- supabase/migrations/20260306061117_add_sports_array_to_athlete_profiles.sql
+++
@@ -1,9 +0,0 @@
--- Add sports TEXT[] column to athlete_profiles
-ALTER TABLE athlete_profiles ADD COLUMN IF NOT EXISTS sports TEXT[] DEFAULT '{}';
-
--- Backfill existing sport to sports array
-UPDATE athlete_profiles
-SET sports = ARRAY[sport]
-WHERE sport IS NOT NULL 
-  AND sport != '' 
-  AND (sports IS NULL OR sports = '{}');
